7a91b277d255fc58befc5bcff85f42029e4f15a0
nworb9 <emma.nworb@gmail.com>
2022-02-08 10:03:29 -0500

    Init repo

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./styles.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="title">
+        <h1>Zesty</h1>
+        <h2>
+          A photography project <br /> by Ella Fieldling
+        </h2>
+      </div>
+      <div className="image-container"></div>
+      <img alt="" src="" />
+    </div>
+  );
+};
+
+export default App;
