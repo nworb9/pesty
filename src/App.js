b5a24c62d179200e808708643c47db637b64512e
nworb9 <emma.nworb@gmail.com>
2022-03-19 18:33:41 -0400

    Greatly improved the theme

--- src/App.js
+++ src/App.js
@@ -1,42 +1,42 @@
 import "./styles.css";
 import React, { useState } from "react";
-import cabbage from "./assets/image1.jpeg";
-import mango from "./assets/image2.jpeg";
-import fig from "./assets/image3.jpeg";
-import gaze from "./assets/image4.jpeg";
-import peach from "./assets/image5.jpeg";
-import avocado from "./assets/image6.jpeg";
+import morning from "./assets/image1.jpeg";
+import pair from "./assets/image2.jpeg";
+import hair from "./assets/image3.jpeg";
+import teeth from "./assets/image4.jpeg";
+import squad from "./assets/image5.jpeg";
+import king from "./assets/image6.png";
 
-const images = [cabbage, mango, fig, gaze, peach, avocado];
+const images = [morning, pair, hair, teeth, squad, king];
 
 const App = () => {
   const [currentImage, setCurrentImage] = useState(0);
 
   const handleClick = () => {
-    alert("hey");
-  };
-
-  const handleChange = (event) => {
-    console.log(event.target.value);
+    const length = images.length - 1;
+    setCurrentImage((currentImage) => {
+      if (currentImage < length) {
+        return currentImage + 1;
+      } else {
+        return 0;
+      }
+    });
   };
 
   return (
     <div className="App">
       <div className="title">
-        <h1>Pesty</h1>
+        <h1>pigsty</h1>
         <h2>
-          a photography project <br /> by Ella Fieldling
+          a photography collection <br /> curated by emma brown
         </h2>
-        <button onClick={handleClick}>click here</button>
-        <input
-          type="text"
-          name="example"
-          autoComplete="off"
-          onChange={handleChange}
-        ></input>
       </div>
       <div className="image-container"></div>
-      <img alt="" src={images[0]} />
+      <img
+        alt="a carousel of pig pictures"
+        src={images[currentImage]}
+        onClick={handleClick}
+      />
     </div>
   );
 };
