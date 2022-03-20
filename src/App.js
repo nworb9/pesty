7272029c46929c6a89fa91a7bb7dcbdb0c076383
nworb9 <emma.nworb@gmail.com>
2022-03-19 22:17:16 -0400

    Slight refactor

--- src/App.js
+++ src/App.js
@@ -4,10 +4,17 @@ import morning from "./assets/image1.jpeg";
 import pair from "./assets/image2.jpeg";
 import hair from "./assets/image3.jpeg";
 import teeth from "./assets/image4.jpeg";
-import squad from "./assets/image5.jpeg";
+import gang from "./assets/image5.jpeg";
 import king from "./assets/image6.png";
 
-const images = [morning, pair, hair, teeth, squad, king];
+const images = [
+  { name: "morning", img: morning },
+  { name: "pair", img: pair },
+  { name: "hair", img: hair },
+  { name: "teeth", img: teeth },
+  { name: "gang", img: gang },
+  { name: "king", img: king },
+];
 
 const App = () => {
   const [currentImage, setCurrentImage] = useState(0);
@@ -15,28 +22,28 @@ const App = () => {
   const handleClick = () => {
     const length = images.length - 1;
     setCurrentImage((currentImage) => {
-      if (currentImage < length) {
-        return currentImage + 1;
-      } else {
-        return 0;
-      }
+      return currentImage < length ? currentImage + 1 : 0;
     });
   };
 
   return (
     <div className="App">
       <div className="title">
-        <h1>pigsty</h1>
-        <h2>
-          a photography collection <br /> curated by emma brown
-        </h2>
+        <h1>PIGSTY</h1>
+        <h2>curated by emma</h2>
       </div>
-      <div className="image-container"></div>
-      <img
-        alt="a carousel of pig pictures"
-        src={images[currentImage]}
-        onClick={handleClick}
-      />
+      <div className="image-container">
+        <h3>
+          {currentImage + 1} / {images.length}
+        </h3>
+        <div className={images[currentImage].name}>
+          <img
+            alt="a carousel of pig pictures"
+            src={images[currentImage].img}
+            onClick={handleClick}
+          />
+        </div>
+      </div>
     </div>
   );
 };
